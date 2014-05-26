8b20a0f960f57be81b8cdbe404d96627f52b1626
elenamorales <elena.moralesb@gmail.com>
2014-05-26 19:01:41 +0200

    Primer Commit
    Primer commit

---
+++ practicaMiguelyElena/practicaMiguelyElena/Miguel.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace practicaMiguelyElena
+{
+    partial class Miguel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Miguel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 335);
+            this.Name = "Miguel";
+            this.Text = "Miguel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
