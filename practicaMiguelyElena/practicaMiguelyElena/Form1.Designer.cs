a37dfd46d2af4f90acebbba48103825174ba46f8
thefronter <mperezrojas1@gmail.com>
2014-05-28 23:45:50 +0200

    Casi acabado el disenyo del lanzador
    Lanzador de las dos aplicaciones practiamente acabado

--- practicaMiguelyElena/practicaMiguelyElena/Form1.Designer.cs
+++ practicaMiguelyElena/practicaMiguelyElena/Form1.Designer.cs
@@ -31,38 +31,105 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.Calculadora = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.Location = new System.Drawing.Point(48, 321);
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.Location = new System.Drawing.Point(34, 249);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(244, 234);
+            this.button1.Size = new System.Drawing.Size(259, 234);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Miguel";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.Location = new System.Drawing.Point(335, 321);
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 5;
+            this.button2.Location = new System.Drawing.Point(365, 249);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(244, 234);
+            this.button2.Size = new System.Drawing.Size(263, 234);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Elena";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Calculadora
+            // 
+            this.Calculadora.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Calculadora.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Calculadora.Font = new System.Drawing.Font("Segoe Print", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calculadora.Location = new System.Drawing.Point(30, 137);
+            this.Calculadora.Name = "Calculadora";
+            this.Calculadora.Size = new System.Drawing.Size(263, 59);
+            this.Calculadora.TabIndex = 2;
+            this.Calculadora.Text = "Calculadora";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(365, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 59);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ahorcado";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 502);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Miguel";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(365, 502);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Elena";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(598, 59);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Aplicaciones con GitHub";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(618, 624);
+            this.ClientSize = new System.Drawing.Size(668, 596);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Calculadora);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -76,6 +143,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Calculadora;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
